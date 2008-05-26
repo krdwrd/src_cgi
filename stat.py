af79920965b452f952dac794376d8401ad9d9b78
JSS <jss@krdwrd.org>
2008-05-26 06:28:04 +0000

    nicer user status page (use moz specific styles)

--- stat.py
+++ stat.py
@@ -2,15 +2,34 @@
 import krdwrd
 import config
 
+corpora = dict((corpus, krdwrd.get_user_tagged(corpus, config.username)) \
+               for corpus in config.corpora)
+
 print "Content-type: text/html\n"
 
-print "<html><head><title>My KrdWrd Stats</title></head><body>"
+print """<html><head><title>My KrdWrd Stats</title>
+<link rel="stylesheet" type="text/css" href="../krdwrd.css" />
+</head><body>"""
 
-print "<h2>%s's KrdWrd Stats</h2>" % config.username
+print """<div class="corpus"><h2>%s's KrdWrd Stats</h2>""" % config.username
+for corpus, pages in corpora.items():
+    clen = len(krdwrd.get_pages(corpus))
+    plen = pages and len(pages) or 0
+    print """<p><a href="#%s">%s</a> : """ % (corpus, corpus,)
+    print plen
+    print " of "
+    print clen
+    print " - "
+    if clen > plen:
+        print clen - plen
+        print " to go. "
+    else:
+        print "done!"
+    print "</p>"
+print """</div>"""
 
-for corpus in config.corpora:
-    print "<h3>Corpus: ", corpus, "</h3>"
-    pages = krdwrd.get_user_tagged(corpus, config.username)
+for corpus, pages in corpora.items():
+    print """<div class="corpus"><h3><a name="%s" />%s</h3>""" % (corpus, corpus,)
     if pages:
       print "<ul>"
       for i, page in reversed(list(enumerate(pages))):
@@ -19,6 +38,6 @@ for corpus in config.corpora:
       print "</ul>"
     else:
       print "no annotations"
-
+    print "</div>"
 
 print "</body></html>"
