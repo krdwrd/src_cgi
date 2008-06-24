14a9409bd44964986a0bb740bcfcf32d728682f9
JSS <jss@krdwrd.org>
2008-06-24 14:05:29 +0000

    optionally serve page in corpus listing order

--- serve.py
+++ serve.py
@@ -9,16 +9,22 @@ fs = cgi.FieldStorage()
 
 corpus = fs.getfirst("corpus", "test")
 
+serial = bool(fs.getfirst("serial"))
+
 pages = krdwrd.get_pages(corpus)
 
 userpages = krdwrd.get_user_tagged(corpus, config.username)
 
 if pages:
-    pages = set(pages) - set(userpages and userpages or [])
 
-if pages:
+  if not serial:
+    pages = set(pages) - set(userpages and userpages or [])
     page = random.sample(pages, 1)[0]
-    loc = "%s/%s" % (config.srcurl(corpus), page)
+  else:
+    page = pages[len(userpages)]
+
+  loc = "%s/%s" % (config.srcurl(corpus), page)
+
 else:
     loc = config.baseurl + 'bin/stat'
 
