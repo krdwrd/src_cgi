2fa83d1d026ba9c02ec155546b506b5a397e66fb
JSS <jss@krdwrd.org>
2008-07-01 10:53:12 +0000

    provide cmd line help

--- dumppage.py
+++ dumppage.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 
-# usage: dumppage page_id prefix
-# will write prefix.user_id files for every submission of page_id
 
 import kwdb
 import sys
 from codecs import open
+
+if len(sys.argv) != 3:
+    print "usage: dumppage page_id prefix"
+    print "will write prefix.user_id files for every submission of page_id"
+    sys.exit(1)
 
 id = int(sys.argv[1])
 out = sys.argv[2]
