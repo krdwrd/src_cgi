57bbe9a82e355dead0c4d3a03bb87bdf7f2d009a
JSS <jss@krdwrd.org>
2008-07-05 15:16:28 +0000

    allow per-user dump of submissions

--- dumppage.py
+++ dumppage.py
@@ -5,17 +5,29 @@ import kwdb
 import sys
 from codecs import open
 
-if len(sys.argv) != 3:
-    print "usage: dumppage page_id prefix"
-    print "will write prefix.user_id files for every submission of page_id"
+if len(sys.argv) < 3:
+    print "usage: dumppage page_id prefix [user_id]"
+    print "write ``prefix``.user_id files for every submission of ``page_id``"
+    print "OR just ``prefix`` if user_id is given"
     sys.exit(1)
 
 id = int(sys.argv[1])
 out = sys.argv[2]
+user_id = len(sys.argv) > 3 and sys.argv[3] or None
 
-for content, user in kwdb.get_all_submissions(id):
-    f = open("%s.%06d" % (out, user,), 'w', 'utf8')
+def dropall():
+    for content, user in kwdb.get_all_submissions(id):
+        f = open("%s.%06d" % (out, user,), 'w', 'utf8')
+        f.write(content)
+        f.close()
+
+def dropuser():
+    content = kwdb.get_subm_content(id, user_id)
+    f = open(out, 'w', 'utf8')
     f.write(content)
     f.close()
 
-
+if not user_id:
+    dropall()
+else:
+    dropuser()
