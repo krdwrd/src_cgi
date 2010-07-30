ebef56ff46199dfa6d094302680376adc816a4a3
Egon W. Stemle <egon@krdwrd.org>
2010-07-30 19:42:42 +0000

    embedded head and body between html tags when serving saved pages from the krdwrd host - otherwise, slight render differences occur frequently

--- viewpage.py
+++ viewpage.py
@@ -7,6 +7,11 @@ if config.path:
     page = int(config.path)
     mime = kwdb.get_page_mime;
     print """Content-type: %s\n""" % (mime) 
+    print """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+"""
     print kwdb.get_page_content(page),
+    print """</html>"""
 else:
     print "Status: 404\n"
